4497dff3d4dad2c985152f4d380f51eee7b500bd
naveenkumar42128 <naveenkumarpk022005@gmail.com>
2026-07-28 10:47:58 +0530

    clean: remove maternal-monitor submodule

--- maternal-monitor/backend/models/Mother.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const motherSchema = new mongoose.Schema({
-  name:     { type: String, required: true },
-  age:      { type: Number, required: true },
-  lmp_date: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Mother", motherSchema);
